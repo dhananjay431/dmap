bceae9d4e4f4528a003973b89988530ac65b6118
dhananjaybelhekar <dhananjay.b@adnatesolutions.com>
2018-08-14 23:11:54 +0530

    add min.js livetrack

--- js/dmaps.js
+++ js/dmaps.js
@@ -7,9 +7,9 @@ this.dmap = {} || [];
             zoom: zoom
         });
     },
-        dmap.mapCenter = function (map, latLng) {
-            map.setCenter(latLng);
-        };
+    dmap.mapCenter = function (map, latLng) {
+        map.setCenter(latLng);
+    };
     dmap.mark = function (map, myLatLng, title = '', image = null) {
         return new google.maps.Marker({
             position: myLatLng,
@@ -56,8 +56,8 @@ this.dmap = {} || [];
         var dLat = (p2.lat - p1.lat) * Math.PI / 180;
         var dLong = (p2.lng - p1.lng) * Math.PI / 180
         var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.cos((p1.lat) * Math.PI / 180) * Math.cos((p2.lat) * Math.PI / 180) *
-            Math.sin(dLong / 2) * Math.sin(dLong / 2);
+        Math.cos((p1.lat) * Math.PI / 180) * Math.cos((p2.lat) * Math.PI / 180) *
+        Math.sin(dLong / 2) * Math.sin(dLong / 2);
         var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
         var d = R * c;
         return d / 1000; // returns the distance in meter
@@ -71,49 +71,43 @@ this.dmap = {} || [];
             dmap.flag = 'stop';
 
         } else
-            if (dmap.flag == 'start') {
-                dmap._moveMarker();
-                dmap.flag = 'stop';
-            } else
-                if (dmap.flag == 'stop') {
+        if (dmap.flag == 'start') {
+            dmap._moveMarker();
+            dmap.flag = 'stop';
+        } else
+        if (dmap.flag == 'stop') {
                     //  dmap.speed = 0;
                     clearTimeout(this.start);
                     dmap.flag = 'start';
                 }
-        if (dmap.flag == 'reset') {
-            mark.setPosition({ lat: coords[0].lat, lng: coords[0].lng });
-            dmap.flag = 'init';
-        }
-        return dmap.flag;
-    };
-    dmap.show = function () { console.log("show"); }
-    dmap.init = function () {
-        dmap.show();
-    }
-    dmap._goToPoint; dmap._moveMarker;
-    dmap.speed = 50;
-    dmap.delay = 100;
-    dmap.marker;
-    dmap.speedInc = function (speed) {
-        dmap.speed += speed;
-        console.log(dmap.speed);
-    }
-    dmap.speedDec = function (speed) {
-        if(dmap.speed>50)
-        dmap.speed -= speed;
-    }
-    dmap.animateMarker = function (map, mark, icons, coords) {
-        var target = 0;
-        // dmap.marker = new google.maps.Marker({
-        //     position: {lat:coords[0].lat,lng:coords[0].lng},
-        //     map: map,
-        //     icon: icons.car
-        // });
-        //       coords.push({lat:coords[coords.length-1].lat,lng:coords[coords.length-1].lng});
-        dmap._goToPoint = function () {
-            // dmap.speed = km_h;
-            var lat = mark.position.lat();
-            var lng = mark.position.lng();
+                if (dmap.flag == 'reset') {
+                    mark.setPosition(coords[0]);
+                    dmap.flag = 'init';
+                }
+                return dmap.flag;
+            };
+            dmap.show = function () { console.log("show"); }
+            dmap.init = function () {
+                dmap.show();
+            }
+            dmap._goToPoint; dmap._moveMarker;
+            dmap.speed = 50;
+            dmap.delay = 100;
+            dmap.marker;
+            dmap.head=[0];
+
+            dmap.speedInc = function (speed) {
+                dmap.speed += speed;
+            }
+            dmap.speedDec = function (speed) {
+                if(dmap.speed>50)
+                    dmap.speed -= speed;
+            }
+            dmap.animateMarker = function (map, mark, icons, coords) {
+                var target = 0;
+                dmap._goToPoint = function () {
+                    var lat = mark.position.lat();
+                    var lng = mark.position.lng();
             var step = (dmap.speed * 1000 * dmap.delay) / 3600000; // in meters
             var dest = new google.maps.LatLng(coords[target].lat, coords[target].lng);
             var distance = google.maps.geometry.spherical.computeDistanceBetween(dest, mark.position); // in meters
@@ -121,9 +115,51 @@ this.dmap = {} || [];
             var i = 0;
             var deltaLat = (coords[target].lat - lat) / numStep;
             var deltaLng = (coords[target].lng - lng) / numStep;
-            // that.speedmeterVal= that.mapData1[target]?(that.mapData1[target][2] + "Kmph"):0;
-            //    console.log('veh speed',that.speedmeterVal)
             dmap._moveMarker = function () {
+                lat += deltaLat;
+                lng += deltaLng;
+                i += step;
+                if (i < distance) {
+                    var head = google.maps.geometry.spherical.computeHeading(mark.getPosition(), new google.maps.LatLng(lat, lng))
+                    icons.car.rotation=Math.floor(head/10)*10;
+                    console.log(icons.car.rotation);
+                    mark.setIcon(icons.car);
+                    mark.setPosition(new google.maps.LatLng(lat, lng));
+                    map.setCenter({ lat: lat, lng: lng });
+                    dmap.start = setTimeout(dmap._moveMarker, dmap.delay);
+                }
+                else {
+                    var head = google.maps.geometry.spherical.computeHeading(mark.getPosition(), new google.maps.LatLng(lat, lng));
+                    icons.car.rotation=head;
+                    mark.setIcon(icons.car);
+                    mark.setPosition(dest);
+                    map.setCenter({ lat: lat, lng: lng });
+                    target++;
+                    if (target == coords.length) {
+                        dmap.flag = 'reset';
+                        clearTimeout(dmap.start);
+                    }
+                    dmap.start = setTimeout(dmap._goToPoint, dmap.delay);
+                }
+            }
+            dmap._moveMarker();
+        }
+        dmap._goToPoint();
+    }
+    dmap.liveTrack =  function(map, mark, icons, coords,speed,delay) {
+        var target = 0;
+        function _goToPoint() {
+            // dmap.speed = km_h;
+            var lat = mark.position.lat();
+            var lng = mark.position.lng();
+            var step = (speed * 1000 * delay) / 3600000; // in meters
+            var dest = new google.maps.LatLng(coords[target].lat, coords[target].lng);
+            var distance = google.maps.geometry.spherical.computeDistanceBetween(dest, mark.position); // in meters
+            var numStep = distance / step;
+            var i = 0;
+            var deltaLat = (coords[target].lat - lat) / numStep;
+            var deltaLng = (coords[target].lng - lng) / numStep;
+            function _moveMarker () {
                 lat += deltaLat;
                 lng += deltaLng;
                 i += step;
@@ -134,8 +170,7 @@ this.dmap = {} || [];
                     }
                     mark.setIcon(icons.car);
                     mark.setPosition(new google.maps.LatLng(lat, lng));
-                    map.setCenter({ lat: lat, lng: lng });
-                    dmap.start = setTimeout(dmap._moveMarker, dmap.delay);
+                    setTimeout(_moveMarker,delay);
                 }
                 else {
                     var head = google.maps.geometry.spherical.computeHeading(mark.getPosition(), new google.maps.LatLng(lat, lng));
@@ -144,20 +179,13 @@ this.dmap = {} || [];
                     }
                     mark.setIcon(icons.car);
                     mark.setPosition(dest);
-                    map.setCenter({ lat: lat, lng: lng });
                     target++;
-                    if (target == coords.length) {
-                        // target = 0;
-                        dmap.flag = 'reset';
-                        clearTimeout(dmap.start);
-                    }
-
-                    dmap.start = setTimeout(dmap._goToPoint, dmap.delay);
+                    setTimeout(_goToPoint,delay);
                 }
             }
-            dmap._moveMarker();
+            _moveMarker();
         }
-        dmap._goToPoint();
+        _goToPoint();
     }
     return dmap;
 })(this.dmap);
